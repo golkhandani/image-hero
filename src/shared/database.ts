741bc5cf5afb3814c1720f042b56ccba1969632b
MohammadRezaRahimianGolkhandani <site.golkhandani@outlook.com>
2021-05-31 19:38:07 +0430

    fix db env

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -3,12 +3,12 @@ import { Db, MongoClient } from "mongodb";
 
 export class MongoConnection {
     private static instance: Db;
-    private static url = 'mongodb://localhost:27017';
+    private static url = process.env.DB;
 
 
     public static async getInstance(): Promise<Db> {
         if (!this.instance) {
-            const connection = await MongoClient.connect(this.url,{ useUnifiedTopology: true });
+            const connection = await MongoClient.connect(this.url!,{ useUnifiedTopology: true });
             this.instance = connection.db("OnPay");
         }
         return this.instance;
